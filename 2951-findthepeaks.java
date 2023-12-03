3f3eee9060feeafdfe3a45cc1525abdb929f8240
lemonredwood <therohithanand@gmail.com>
2023-12-03 11:04:15 +0530

    aced it

---
+++ 2951-findthepeaks.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> ll = new LinkedList();
+        for(int i=1;i<mountain.length-1;i++){
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]) ll.add(i);
+        }
+        return ll;
+    }
+}

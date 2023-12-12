577edcbef42463ad6f8fe0335b0697091f231e5a
lemonredwood <therohithanand@gmail.com>
2023-12-12 21:26:14 +0530

    updated code

--- 1464-maxproduct.java
+++ 1464-maxproduct.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        fort(nums);
+        Arrays.sort(nums);
         int x = nums[nums.length-1]-1;
         int y = nums[nums.length-2]-1;
         return x*y;

b512fd457dad2703684aa637e77de734250f39a7
lemonredwood <therohithanand@gmail.com>
2023-12-21 18:05:47 +0530

    daily daily

---
+++ 1673-widestarea.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int max = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i + 1][0] - points[i][0] > max) {
+                max = points[i + 1][0] - points[i][0];
+            }
+        }
+        return max;
+    }
+}

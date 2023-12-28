e8d4828edcddab8563f7c2dbccf9b44e186f682d
lemonredwood <therohithanand@gmail.com>
2023-12-29 03:11:05 +0530

    roman to integer

---
+++ 13-romantoint.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int romanToInt(String s) {
+        int num = 0;
+        int i = s.length()-1;
+
+        while(i>=0){
+            if(s.charAt(i)=='M'){
+                if( i!=0 && s.charAt(i-1)=='C'){
+                    num+=900;
+                    i--;
+                }
+                else num+=1000;
+            }
+            else if(s.charAt(i)=='D'){
+                if( i!=0 && s.charAt(i-1)=='C'){
+                    num+=400;
+                    i--;
+                }
+                else num+=500;
+            }
+            else if(s.charAt(i)=='C'){
+                if( i!=0 && s.charAt(i-1)=='X'){
+                    num+=90;
+                    i--;
+                }
+                else num+=100;
+            }
+            else if(s.charAt(i)=='L'){
+                if( i!=0 && s.charAt(i-1)=='X'){
+                    num+=40;
+                    i--;
+                }
+                else num+=50;
+            }
+            else if(s.charAt(i)=='X'){
+                if( i!=0 && s.charAt(i-1)=='I'){
+                    num+=9;
+                    i--;
+                }
+                else num+=10;
+            }
+            else if(s.charAt(i)=='V'){
+                if( i!=0 && s.charAt(i-1)=='I'){
+                    num+=4;
+                    i--;
+                }
+                else num+=5;
+            }
+            else if(s.charAt(i)=='I')num+=1;
+            i--;
+        }
+       
+        return num;
+    }
+}

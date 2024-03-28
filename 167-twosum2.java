1d76376e9c15516213eb5e8fc21c46c694686d7e
lemonredwood <therohithanand@gmail.com>
2024-03-28 23:16:07 +0530

    efficiency is key

--- 167-twosum2.java
+++ 167-twosum2.java
@@ -23,3 +23,24 @@ class Solution {
         return ans;
     }
 }
+
+
+// Better Efficient Solution
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length-1;
+        int ans[] = new int[2];
+        while(l<r){
+            if(numbers[l]+numbers[r]==target){
+                ans[0]=l+1;
+                ans[1]=r+1;
+                return ans;
+            }
+            else if(numbers[l]+numbers[r]>target)r--;
+            else l++;
+        }
+        return ans;
+    }
+}
